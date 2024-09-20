85cdf94fb05b6e3ba87e463b1941ea2914850bd5
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-21 01:19:08 +0300

    Dto Name Update

--- Services/IDynamicObjectService.cs
+++ Services/IDynamicObjectService.cs
@@ -1,12 +1,13 @@
 ﻿using MicromarinCase.Dtos;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace MicromarinCase.Services
 {
     public interface IDynamicObjectService
     {
         Task<List<ResultDto>> GettAllDynamicObjectAsync();
-        Task CreateDynamicObjectAsync(CreateDto createDynamicObjectDto);
-        Task UpdateDynamicObjectAsync(UpdateDto updateDynamicObjectDto);
+        Task CreateDynamicObjectAsync(CreateDto createDto);
+        Task UpdateDynamicObjectAsync(UpdateDto updateDto);
         Task DeleteOrderAsync(int Id);
     }
 }
