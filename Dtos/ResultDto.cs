3b7a47036735aefe86f0001f541b8c5ba091384a
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-20 17:17:47 +0300

    Result and Update Dtos Created

---
+++ Dtos/ResultDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MicromarinCase.Dtos
+{
+    public class ResultDto
+    {
+        public int Id { get; set; }
+        public string JsonData { get; set; }
+
+    }
+}
---
+++ Dtos/UpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MicromarinCase.Dtos
+{
+    public class UpdateDto
+    {
+        public int Id { get; set; }
+        public Dictionary<string, object> DynamicObject { get; set; }
+        public List<Dictionary<string, object>> DynamicSubObject { get; set; }
+    }
+}
