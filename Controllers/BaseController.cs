2ee67534a07904cf956b72a13ab4bae4d32ce535
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-20 15:35:21 +0300

    Created Base Controller and Create Method

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,31 @@
+﻿using MicromarinCase.Dtos;
+using MicromarinCase.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicromarinCase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private readonly IDynamicObjectService _dynamicObjectService;
+        public BaseController(IDynamicObjectService dynamicObjectService)
+        {
+            _dynamicObjectService = dynamicObjectService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDynamicObject(CreateOrderDto createOrderDto)
+        {
+            try
+            {
+                await _dynamicObjectService.CreateOrderAsync(createOrderDto);
+                return Ok("Dynamic object successfully added");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
