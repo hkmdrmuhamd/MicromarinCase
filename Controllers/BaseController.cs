609918c4cf7adc4d52513009d14652ac9bcd983a
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-20 17:20:20 +0300

    Result - Updates and Delete Operations Added

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -14,13 +14,55 @@ namespace MicromarinCase.Controllers
             _dynamicObjectService = dynamicObjectService;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetAllDynamicObject()
+        {
+            try
+            {
+                var result = await _dynamicObjectService.GettAllDynamicObjectAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
         [HttpPost]
         public async Task<IActionResult> CreateDynamicObject(CreateDto createOrderDto)
         {
             try
             {
-                await _dynamicObjectService.CreateOrderAsync(createOrderDto);
+                await _dynamicObjectService.CreateDynamicObjectAsync(createOrderDto);
                 return Ok("Dynamic object successfully added");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateDynamicObject(UpdateDto updateOrderDto)
+        {
+            try
+            {
+                await _dynamicObjectService.UpdateDynamicObjectAsync(updateOrderDto);
+                return Ok("Dynamic object successfully updated");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDynamicObject(int Id)
+        {
+            try
+            {
+                await _dynamicObjectService.DeleteOrderAsync(Id);
+                return Ok("Dynamic object successfully deleted");
             }
             catch (Exception ex)
             {
