9ecf4bb8893883aaa14dc29bda6dcef83b5b6187
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-21 01:18:51 +0300

    Ignore fields with BindNever

--- Dtos/CreateDto.cs
+++ Dtos/CreateDto.cs
@@ -1,8 +1,12 @@
-﻿namespace MicromarinCase.Dtos
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MicromarinCase.Dtos
 {
     public class CreateDto
     {
+        [BindNever]
         public Dictionary<string, object> DynamicObject { get; set; }
+        [BindNever]
         public List<Dictionary<string, object>> DynamicSubObject { get; set; }
     }
 }
--- Dtos/UpdateDto.cs
+++ Dtos/UpdateDto.cs
@@ -1,9 +1,13 @@
-﻿namespace MicromarinCase.Dtos
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MicromarinCase.Dtos
 {
     public class UpdateDto
     {
         public int Id { get; set; }
+        [BindNever]
         public Dictionary<string, object> DynamicObject { get; set; }
+        [BindNever]
         public List<Dictionary<string, object>> DynamicSubObject { get; set; }
     }
 }
