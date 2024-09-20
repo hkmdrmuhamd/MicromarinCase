7a9709f9cec684672a07b8faddfd5eaf32a18384
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-21 02:06:41 +0300

    Removal of unnecessary entities

--- Entities/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MicromarinCase.Entities
-{
-    public abstract class BaseEntity
-    {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public Dictionary<string, object> DynamicFields { get; set; } = new Dictionary<string, object>();
-    }
-}
