1700c84117182cf9b5027fad6d9ed52f0be0e13c
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-20 15:33:25 +0300

    Dtos Created

---
+++ Dtos/CreateOrderDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MicromarinCase.Dtos
+{
+    public class CreateOrderDto
+    {
+        public Dictionary<string, object> OrderData { get; set; }
+        public List<Dictionary<string, object>> Products { get; set; }
+    }
+}
