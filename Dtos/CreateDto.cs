11c7cd17c5c070d891ed17fca72a3ae95e339ca5
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-09-22 15:19:17 +0300

    Remove data annotations from properties

--- Dtos/CreateDto.cs
+++ Dtos/CreateDto.cs
@@ -4,9 +4,7 @@ namespace MicromarinCase.Dtos
 {
     public class CreateDto
     {
-        [BindNever]
         public Dictionary<string, object> DynamicObject { get; set; }
-        [BindNever]
         public List<Dictionary<string, object>> DynamicSubObject { get; set; }
     }
 }
--- Dtos/UpdateDto.cs
+++ Dtos/UpdateDto.cs
@@ -5,9 +5,7 @@ namespace MicromarinCase.Dtos
     public class UpdateDto
     {
         public int Id { get; set; }
-        [BindNever]
         public Dictionary<string, object> DynamicObject { get; set; }
-        [BindNever]
         public List<Dictionary<string, object>> DynamicSubObject { get; set; }
     }
 }
